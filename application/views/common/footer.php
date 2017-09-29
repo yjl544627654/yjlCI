6c4c3afc383919e0e3b49ad8d6c51a0779463db7
yjl544627654 <544627654@qq.com>
2017-09-29 16:47:20 +0800

    加入amazeUI前端
    做了头部 菜单 内容的视图分离

---
+++ application/views/common/footer.php
@@ -0,0 +1,11 @@
+ 	
+
+ 	</div>
+    <script src="<?php echo base_url();?>assets/js/amazeui.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/amazeui.datatables.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/dataTables.responsive.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/app.js"></script>
+
+</body>
+
+</html>
