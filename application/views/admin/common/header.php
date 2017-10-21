1aaa608a7901fb10e9740dc74aef94913aa850b4
yjl544627654 <544627654@qq.com>
2017-10-21 15:52:18 +0800

    后台调整
    后台放进控制器文件下的admin目录
    models/admin
    view/admin后台文件添加

---
+++ application/views/admin/common/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>yjlCI后台管理</title>
+    <meta name="description" content="这是一个 yjlCI后台管理 页面">
+    <meta name="keywords" content="index">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="Cache-Control" content="no-siteapp" />
+    <link rel="icon" type="image/png" href="<?php echo PUBLIC_PATH;?>assets/i/favicon.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo PUBLIC_PATH;?>assets/i/app-icon72x72@2x.png">
+    <meta name="apple-mobile-web-app-title" content="Amaze UI" />
+    <link rel="stylesheet" href="<?php echo PUBLIC_PATH;?>assets/css/amazeui.min.css" />
+    <link rel="stylesheet" href="<?php echo PUBLIC_PATH;?>assets/css/amazeui.datatables.min.css" />
+    <link rel="stylesheet" href="<?php echo PUBLIC_PATH;?>assets/css/app.css">
+    <script src="<?php echo PUBLIC_PATH;?>assets/js/jquery.min.js"></script>
+
+
+</head>
+
+<body data-type="index">
+    <script src="<?php echo PUBLIC_PATH;?>assets/js/theme.js"></script>
+    <div class="am-g tpl-g">
+
+    <?php $this->load->view('admin/common/top'); ?>
+
+    <?php $this->load->view('admin/common/left'); ?>    
